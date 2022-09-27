0fafa718dfbc69001ae1089832eb674fd6993165
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-27 03:16:28 +0000

    ?

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
 gem 'devise'
+gem "active_storage_validations"
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
  devise_for :users
   root 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
-  end
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
